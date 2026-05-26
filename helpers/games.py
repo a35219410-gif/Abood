d2392e54cf706532775d2397875713e629a7a07b
a35219410-gif <a35219410@gmail.com>
2026-05-27 01:23:28 +0300

    upload helpers/games.py

---
+++ helpers/games.py
@@ -0,0 +1,9 @@
+import random
+def get_emoji_bank(a):
+    if a>=10000: return "💎"
+    if a>=5000: return "🏆"
+    if a>=1000: return "💰"
+    if a>=500: return "🪙"
+    return "💵"
+def get_word_game_words():
+    return ["تفاحة","موز","برتقال","عنب","مانجو","كمبيوتر","هاتف","سيارة","طائرة","شمس","قمر","نجمة","سحابة","مطر","أسد","نمر","فيل","زرافة","قرد","أرنب"]
